1e05fc68651ddad05bcefeaea1c8511d40c1ad12
Azuusiek <azuusiek@gmail.com>
2023-11-14 13:23:57 +0100

    weather app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,137 +1,118 @@
-let map = L.map('map').setView([53.430127, 14.564802], 18);
-// L.tileLayer.provider('OpenStreetMap.DE').addTo(map);
-L.tileLayer.provider('Esri.WorldImagery').addTo(map);
-let marker = L.marker([53.430127, 14.564802]).addTo(map);
-marker.bindPopup("<strong>Hello!</strong><br>This is a popup.");
+const Weather = class {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+        this.currentWeatherLink = `https://api.openweathermap.org/data/2.5/weather?q={query}&appid=${apiKey}&units=metric&lang=pl`;
+        this.forecastLink = `https://api.openweathermap.org/data/2.5/forecast?q={query}&appid=${apiKey}&units=metric&lang=pl`;
+        this.iconLink = "https://openweathermap.org/img/wn/{iconName}@2x.png";
 
-document.getElementById("splitButton").addEventListener("click", function() {
-    leafletImage(map, function (err, canvas) {
-        let table = document.getElementById("table");
-        table.textContent = '';
-        let index = 1;
-        for(i = 1; i<=3; i++) {
-            for(j = 1; j<=3; j++) {
-                let canvasPart = document.createElement("canvas");
-                let xPart = (canvas.width / 3);
-                let yPart = (canvas.height / 3);
-                canvasPart.height =  (canvas.width / 3);
-                canvasPart.width =  (canvas.height / 3);
-                canvasPart.setAttribute("puzzleIndex", index);
-                canvasPart.setAttribute("id", "draggable-item-"+index);
-                canvasPart.setAttribute("draggable", true);
-                canvasPart.classList.add("item");
-                canvasPart.classList.add("draggable");
-                index++;
-                let canvasContext = canvasPart.getContext("2d");
-                let singleContext = table.appendChild(canvasPart);
-                canvasContext.drawImage(canvas, (j-1) * xPart, (i-1) * yPart, xPart, yPart, 0, 0, xPart, yPart);
+        this.currentWeather = undefined;
+        this.forecast = undefined;
+        this.resultBlockSelector = "#result";
+        this.resultsBlock = document.querySelector(this.resultBlockSelector);
+    };
+
+    getCurrWeather(value) {
+        let url = this.currentWeatherLink.replace("{query}", value);
+        let req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.addEventListener("load", () => {
+            this.currentWeather = JSON.parse(req.responseText);
+            this.draw();
+        });
+        req.send();
+    }
+    getForecast(value) {
+        let url = this.forecastLink.replace("{query}", value);
+        fetch(url).then((response) => {
+            return response.json();
+        }).then((data) => {
+            this.forecast = data.list;
+            this.draw();
+        });
+    }
+
+    getWeather(value) {
+        // maybe better solution its make promises of these two functions instead drawing twice?
+        this.getCurrWeather(value);
+        this.getForecast(value);
+    }
+
+    createBlock(dateString, temp, feelsLikeTemp, iconName, description) {
+        let weatherBlock = document.createElement("div");
+        weatherBlock.className = "block";
+
+        let dateBlock = document.createElement("div");
+        dateBlock.className = "date";
+        dateBlock.innerText = dateString;
+        weatherBlock.appendChild(dateBlock);
+
+        let tempBlock = document.createElement("div");
+        tempBlock.className = "temp";
+        tempBlock.innerHTML = `${temp} &deg;C`;
+        weatherBlock.appendChild(tempBlock);
+
+        let feelsLikeBlock = document.createElement("div");
+        feelsLikeBlock.className = "feels-like";
+        feelsLikeBlock.innerHTML = `Odczuwalna: ${feelsLikeTemp} &deg;C`;
+        weatherBlock.appendChild(feelsLikeBlock);
+
+        let weatherIcon = document.createElement("img");
+        weatherIcon.className = "icon";
+        weatherIcon.src = this.iconLink.replace("{iconName}", iconName);
+        weatherBlock.appendChild(weatherIcon);
+
+        let weatherDescription = document.createElement("div");
+        weatherDescription.className = "description";
+        weatherDescription.innerText = description;
+        weatherBlock.appendChild(weatherDescription);
+        weatherBlock.className = "block";
+        return weatherBlock;
+    }
+    clear() {
+        this.resultsBlock.innerHTML = '';
+    }
+    draw() {
+        this.clear();
+        let currentBlock = document.createElement("div");
+        currentBlock.id = "current";
+        //currentBlock.className = "block";
+        this.resultsBlock.appendChild(currentBlock);
+        if (this.currentWeather) {
+            const date = new Date(this.currentWeather.dt * 1000);
+            const dateTimeString = `${date.toLocaleDateString("pl-PL")} ${date.toLocaleTimeString("pl-PL")}`;
+
+            const temperature = this.currentWeather.main.temp;
+            const feelsLikeTemperature = this.currentWeather.main.feels_like;
+            const iconName = this.currentWeather.weather[0].icon;
+            const description = this.currentWeather.weather[0].description;
+
+            const weatherBlock = this.createBlock(dateTimeString, temperature, feelsLikeTemperature, iconName, description);
+            currentBlock.appendChild(weatherBlock);
+        }
+
+        let forecastBlock = document.createElement("div");
+        forecastBlock.id = "forecast";
+        this.resultsBlock.appendChild(forecastBlock);
+        if (this.forecast && this.forecast.length > 0) {
+            for (let i = 0; i < this.forecast.length; i++) {
+                let weather = this.forecast[i];
+                const date = new Date(weather.dt * 1000);
+                const dateTimeString = `${date.toLocaleDateString("pl-PL")} ${date.toLocaleTimeString("pl-PL")}`;
+
+                const temperature = weather.main.temp;
+                const feelsLikeTemperature = weather.main.feels_like;
+                const iconName = weather.weather[0].icon;
+                const description = weather.weather[0].description;
+
+                const weatherBlock = this.createBlock(dateTimeString, temperature, feelsLikeTemperature, iconName, description);
+                forecastBlock.appendChild(weatherBlock);
             }
         }
-        //shuffling elements
-        var divs = table.children;
-        var frag = document.createDocumentFragment();
-        while (divs.length) {
-            frag.appendChild(divs[Math.floor(Math.random() * divs.length)]);
-        }
-        table.appendChild(frag);
-        refreshDragging();
-    });
+    }
+}
+const API_KEY = "7ded80d91f2b280ec979100cc8bbba94";
+let weatherApp = new Weather(API_KEY);
+document.querySelector("#getWeatherButton").addEventListener("click", function() {
+    const query = document.querySelector("#location").value;
+    weatherApp.getWeather(query);
 });
-document.getElementById("getLocation").addEventListener("click", function(event) {
-    if (! navigator.geolocation) {
-        console.log("No geolocation.");
-    }
-
-    navigator.geolocation.getCurrentPosition(position => {
-        console.log(position);
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
-
-        map.setView([lat, lon]);
-    }, positionError => {
-        console.error(positionError);
-    });
-});
-
-const showNotification = () => {
-    if(Notification.permission === 'granted') {
-    const notification = new Notification('JavaScript Notification API', {
-        body: 'Puzzles done!',
-    });
-
-    setTimeout(() => {
-        notification.close();
-    }, 10 * 1000);
-
-    notification.addEventListener('click', () => {
-        // do nothing
-    });
-}
-}
-(async () => {
-/*
-    const showError = () => {
-        const error = document.querySelector('.error');
-        error.style.display = 'block';
-        error.textContent = 'You blocked the notifications';
-    }
-*/
-    let granted = false;
-
-    if (Notification.permission === 'granted') {
-        granted = true;
-    } else if (Notification.permission !== 'denied') {
-        let permission = await Notification.requestPermission();
-        granted = permission === 'granted' ? true : false;
-    }
-    /*
-    granted ? showNotification() : showError();
-    */
-    console.log("notification granted" + granted);
-})();
-function checkPuzzles() {
-    let table = document.getElementById("targetPuzzles");
-    let items = table.querySelectorAll('.item');
-    let checking = [];
-    for (let item of items) {
-        checking.push(parseInt(item.getAttribute("puzzleindex")));
-    }
-    console.log(checking);
-    if(arraysEqual(checking,[1,2,3,4,5,6,7,8,9]))
-        showNotification();
-}
-function arraysEqual(a1,a2) {
-    return JSON.stringify(a1)==JSON.stringify(a2);
-}
-function refreshDragging() {
-    let items = document.querySelectorAll('.item');
-    for (let item of items) {
-        item.addEventListener("dragstart", function(event) {
-            this.style.border = "5px dashed #D8D8FF";
-            event.dataTransfer.setData("text", this.id);
-        });
-
-        item.addEventListener("dragend", function(event) {
-            this.style.borderWidth = "0";
-        });
-    }
-
-    let targets = document.querySelectorAll(".drag-target");
-    for (let target of targets) {
-        target.addEventListener("dragenter", function (event) {
-            this.style.border = "2px solid #7FE9D9";
-        });
-        target.addEventListener("dragleave", function (event) {
-            this.style.border = "2px dashed #7f7fe9";
-        });
-        target.addEventListener("dragover", function (event) {
-            event.preventDefault();
-        });
-        target.addEventListener("drop", function (event) {
-            let myElement = document.querySelector("#" + event.dataTransfer.getData('text'));
-            this.appendChild(myElement)
-            this.style.border = "2px dashed #7f7fe9";
-            checkPuzzles();
-        }, false);
-    }
-}
